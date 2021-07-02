78a0eb3457f0f297d8bc9fa25323bed94b88f20c
KK <1985088337@qq.com>
2021-07-02 23:58:39 +0800

    添加简化扩展方法

--- OSS.PipeLine/SimplePipeline/Builder/SimplePipelineBuilder.cs
+++ OSS.PipeLine/SimplePipeline/Builder/SimplePipelineBuilder.cs
@@ -1,111 +1,97 @@
-﻿//#region Copyright (C) 2020 Kevin (OSS开源系列) 公众号：OSSCore
+﻿#region Copyright (C) 2020 Kevin (OSS开源系列) 公众号：OSSCore
 
-///***************************************************************************
-//*　　	文件功能描述：OSS.EventFlow -  流体生成器
-//*
-//*　　	创建人： Kevin
-//*       创建人Email：1985088337@qq.com
-//*       创建时间： 2020-11-22
-//*       
-//*****************************************************************************/
+/***************************************************************************
+*　　	文件功能描述：OSS.EventFlow -  流体生成器
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*       创建时间： 2020-11-22
+*       
+*****************************************************************************/
 
-//#endregion
+#endregion
 
-//using OSS.Pipeline.Base;
-//using OSS.Pipeline.Interface;
-//using OSS.Pipeline.InterImpls.Pipeline;
-//using System.Collections.Generic;
+using OSS.Pipeline.Base;
+using OSS.Pipeline.Interface;
+using OSS.Pipeline.InterImpls.Pipeline;
+using System.Collections.Generic;
 
-//namespace OSS.Pipeline
-//{
-//    /// <summary>
-//    ///  pipeline 生成器
-//    /// </summary>
-//    public static partial class PipelineBuilder
-//    {
-//        /// <summary>
-//        ///  添加第一个节点
-//        /// </summary>
-//        /// <typeparam name="TPara"></typeparam>
-//        /// <typeparam name="TIn"></typeparam>
-//        /// <typeparam name="TOut"></typeparam>
-//        /// <typeparam name="TResult"></typeparam>
-//        /// <param name="startPipe"></param>
-//        /// <returns></returns>
-//        public static IPipelineAppender<TIn, TOut> Start<TIn, TPara, TResult, TOut>(BaseFourWayPipe<TIn, TPara,TResult, TOut> startPipe)
-//        {
-//            return new InterPipelineAppender<TIn, TOut>( startPipe, startPipe);
-//        }
+namespace OSS.Pipeline
+{
+    /// <summary>
+    ///  pipeline 生成器
+    /// </summary>
+    public static partial class SimplePipelineBuilder
+    {
+        /// <summary>
+        ///  添加第一个节点
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <typeparam name="TContext"></typeparam>
+        /// <param name="startPipe"></param>
+        /// <returns></returns>
+        public static ISimplePipelineAppender<TContext> Start<TContext,  TResult>(BaseThreeWayPipe<TContext, TResult, TContext> startPipe)
+        {
+            return new InterSimplePipelineAppender<TContext>(startPipe, startPipe);
+        }
 
-//        /// <summary>
-//        ///  添加第一个节点
-//        /// </summary>
-//        /// <typeparam name="TPara"></typeparam>
-//        /// <typeparam name="TOut"></typeparam>
-//        /// <typeparam name="TResult"></typeparam>
-//        /// <param name="startPipe"></param>
-//        /// <returns></returns>
-//        public static IPipelineAppender<Empty, TOut> Start< TPara, TResult, TOut>(BaseFourWayPipe<Empty, TPara, TResult, TOut> startPipe)
-//        {
-//            return new InterPipelineAppender<Empty, TOut>( startPipe, startPipe);
-//        }
-        
-//        /// <summary>
-//        ///  添加第一个节点
-//        /// </summary>
-//        /// <typeparam name="TIn"></typeparam>
-//        /// <param name="startPipe"></param>
-//        /// <returns></returns>
-//        public static IPipelineBranchAppender<TIn, TIn> Start<TIn>(BaseBranchGateway<TIn> startPipe)
-//        {
-//            return new InterPipelineBranchAppender<TIn, TIn>( startPipe, startPipe);
-//        }
+        ///// <summary>
+        /////  添加第一个节点
+        ///// </summary>
+        ///// <typeparam name="TPara"></typeparam>
+        ///// <typeparam name="TOut"></typeparam>
+        ///// <typeparam name="TResult"></typeparam>
+        ///// <param name="startPipe"></param>
+        ///// <returns></returns>
+        //public static ISimplePipelineAppender<TContext> Start<TContext, TResult>(BaseThreeWayPassivePipe<Empty, TContext, TResult, TContext> startPipe)
+        //{
+        //    return new InterPipelineAppender<Empty, TOut>(startPipe, startPipe);
+        //}
 
-//        /// <summary>
-//        ///  添加第一个节点
-//        /// ( 消息枚举器
-//        /// </summary>
-//        /// <typeparam name="TMsg"></typeparam>
-//        /// <param name="startPipe"></param>
-//        /// <returns></returns>
-//        public static IPipelineMsgEnumerableAppender<TMsgEnumerable, TMsgEnumerable, TMsg> Start<TMsgEnumerable,TMsg>(BaseMsgEnumerator<TMsgEnumerable,TMsg> startPipe)
-//            where TMsgEnumerable : IEnumerable<TMsg>
-//        {
-//            return new InterPipelineMsgEnumerableAppender<TMsgEnumerable, TMsgEnumerable, TMsg>(startPipe, startPipe);
-//        }
+        ///// <summary>
+        /////  添加第一个节点
+        ///// </summary>
+        ///// <typeparam name="TIn"></typeparam>
+        ///// <param name="startPipe"></param>
+        ///// <returns></returns>
+        //public static ISimplePipelineBranchAppender<TIn, TIn> Start<TIn>(BaseBranchGateway<TIn> startPipe)
+        //{
+        //    return new InterPipelineBranchAppender<TIn, TIn>(startPipe, startPipe);
+        //}
 
-//        /// <summary>
-//        /// 根据首位两个管道建立流体
-//        /// </summary>
-//        /// <typeparam name="InFlowContext"></typeparam>
-//        /// <typeparam name="OutFlowContext"></typeparam>
-//        /// <param name="startPipe"></param>
-//        /// <param name="endPipeAppender"></param>
-//        /// <param name="flowPipeCode"></param>
-//        /// <param name="option"></param>
-//        /// <returns></returns>
-//        public static Pipeline<InFlowContext, OutFlowContext> AsPipelineStartAndEndWith<InFlowContext, OutFlowContext>(this BaseInPipePart<InFlowContext> startPipe, IPipeAppender<OutFlowContext> endPipeAppender,
-//            string flowPipeCode, PipeLineOption option = null)
-//        {
-//            return new Pipeline<InFlowContext, OutFlowContext>(flowPipeCode, startPipe, endPipeAppender, option);
-//        }
 
-//        /// <summary>
-//        ///  追加下一个节点
-//        /// </summary>
-//        /// <typeparam name="TIn"></typeparam>
-//        /// <typeparam name="TOut"></typeparam>
-//        /// <param name="pipe"></param>
-//        /// <param name="pipeCode"></param>
-//        /// <param name="option"></param>
-//        /// <returns></returns>
-//        public static Pipeline<TIn, TOut> AsPipeline<TIn, TOut>(this IPipelineAppender<TIn, TOut> pipe, string pipeCode, PipeLineOption option = null)
-//        {
-//            var newPipe = new Pipeline<TIn, TOut>(pipeCode, pipe.StartPipe, pipe.EndAppender, option);
-//            pipe.StartPipe   = null;
-//            pipe.EndAppender = null;
-//            return newPipe;
-//        }
+        ///// <summary>
+        ///// 根据首位两个管道建立流体
+        ///// </summary>
+        ///// <typeparam name="InFlowContext"></typeparam>
+        ///// <typeparam name="OutFlowContext"></typeparam>
+        ///// <param name="startPipe"></param>
+        ///// <param name="endPipeAppender"></param>
+        ///// <param name="flowPipeCode"></param>
+        ///// <param name="option"></param>
+        ///// <returns></returns>
+        //public static Pipeline<InFlowContext, OutFlowContext> AsPipelineStartAndEndWith<InFlowContext, OutFlowContext>(this BaseInPipePart<InFlowContext> startPipe, ISimplePipeAppender<OutFlowContext> endPipeAppender,
+        //    string flowPipeCode, PipeLineOption option = null)
+        //{
+        //    return new Pipeline<InFlowContext, OutFlowContext>(flowPipeCode, startPipe, endPipeAppender, option);
+        //}
 
-//    }
-//}
+        ///// <summary>
+        /////  追加下一个节点
+        ///// </summary>
+        ///// <typeparam name="TIn"></typeparam>
+        ///// <typeparam name="TOut"></typeparam>
+        ///// <param name="pipe"></param>
+        ///// <param name="pipeCode"></param>
+        ///// <param name="option"></param>
+        ///// <returns></returns>
+        //public static Pipeline<TIn, TOut> AsPipeline<TIn, TOut>(this ISimplePipelineAppender<TIn, TOut> pipe, string pipeCode, PipeLineOption option = null)
+        //{
+        //    var newPipe = new Pipeline<TIn, TOut>(pipeCode, pipe.StartPipe, pipe.EndAppender, option);
+        //    pipe.StartPipe = null;
+        //    pipe.EndAppender = null;
+        //    return newPipe;
+        //}
+
+    }
+}
---
+++ OSS.PipeLine/SimplePipeline/InterImpls/InterSimplePipelineAppender.cs
@@ -0,0 +1,56 @@
+﻿#region Copyright (C) 2020 Kevin (OSS开源系列) 公众号：OSSCore
+
+/***************************************************************************
+*　　	文件功能描述：OSS.EventFlow -  流体追加器内部实现
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*       创建时间： 2020-11-22
+*       
+*****************************************************************************/
+
+#endregion
+
+using OSS.Pipeline.Base;
+using OSS.Pipeline.Interface;
+
+namespace OSS.Pipeline.InterImpls.Pipeline
+{
+    internal class InterSimplePipelineAppender<TContext> : ISimplePipelineAppender<TContext>
+    {
+
+        public InterSimplePipelineAppender(BaseInPipePart<TContext> startPipe, IPipeAppender<TContext> endPipe)
+        {
+            Initial(this, startPipe, endPipe);
+        }
+        
+        private static void Initial(ISimplePipelineAppender<TContext> pipelineAppender,
+            BaseInPipePart<TContext> startPipe, IPipeAppender<TContext> endPipe)
+        {
+            pipelineAppender.StartPipe   = startPipe;
+            pipelineAppender.EndAppender = endPipe;
+        }
+
+        BaseInPipePart<TContext> ISimplePipelineAppender<TContext>.StartPipe { get; set; }
+
+        IPipeAppender<TContext> ISimplePipelineAppender<TContext>.EndAppender { get; set; }
+    }
+
+    internal class InterSimplePipelineBranchAppender<TContext> : ISimplePipelineBranchAppender<TContext>
+    {
+        public InterSimplePipelineBranchAppender(BaseInPipePart<TContext> startPipe, BaseBranchGateway<TContext> endPipe)
+        {
+            Initial(this, startPipe, endPipe);
+        }
+
+        private static void Initial(ISimplePipelineBranchAppender<TContext> pipelineAppender,
+            BaseInPipePart<TContext> startPipe, BaseBranchGateway<TContext> endPipe)
+        {
+            pipelineAppender.StartPipe     = startPipe;
+            pipelineAppender.EndBranchPipe = endPipe;
+        }
+
+        BaseInPipePart<TContext> ISimplePipelineBranchAppender<TContext>.  StartPipe     { get; set; }
+        BaseBranchGateway<TContext> ISimplePipelineBranchAppender<TContext>.EndBranchPipe { get; set; }
+    }
+}
